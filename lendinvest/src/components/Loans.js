60e30e9b2532d265b4c331e35dca5c220d6803b1
Osman Hernandez <osman.sist@gmail.com>
2018-01-28 14:56:47 +0100

    showing loans info and invest button

--- lendinvest/src/components/Loans.js
+++ lendinvest/src/components/Loans.js
@@ -3,16 +3,31 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const getLoans = ({ loans }) => loans.map((loan, index) => {
-	return <div className="ui segment" key={index}>
-		<p>{loan.title}</p>
+	return <div className="ui padded blue segment" key={index}>
+		<div class="ui two column grid">
+			<div className="ui twelve wide  column">
+				<h3 className="ui header">{loan.title}</h3>
+				<div class="meta">
+					<span>
+						<strong> Tranche:</strong> {loan.tranche},
+						<strong> Available: &#163;</strong>  {loan.available},
+						<strong> Amount: &#163;</strong> {loan.amount}
+					</span>
+		      	</div>
+		    </div>
+		    <div className="ui right aligned four wide column">
+		    	<div class="meta">
+		    		<span className="green"><strong>Invested</strong></span>
+		    	</div>
+		      	<div class="ui primary fluid button">Invest in Loan</div>
+	      	</div>
+	    </div>
 	</div>
 });
 
 const Loans = loans => (
 	<div className="column">
-		<div className="ui raised segments">
-			{getLoans(loans)}
-		</div>
+		{getLoans(loans)}
 	</div>
 );
 
